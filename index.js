6d7b63c1bb0ceeeca7e113e136c82fc85eb81f52
khanqdev <khanq.dev@gmail.com>
2024-12-08 11:00:26 +0700

    add logic create/get table, get/insert record

--- index.js
+++ index.js
@@ -4,56 +4,129 @@ const PG = require("pg");
 const app = express();
 const bodyParser = require("body-parser");
 
-var pg_client = null;
-const connectionString = "postgres://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOST + ":" + process.env.PG_PORT + "/" + process.env.DB;
-const pg = new PG.Pool({ connectionString });
+const client = new PG.Client({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.DB
+})
+client.connect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-pg.connect(function (err, client) {
-    if (err) {
-        //. DB not running on first boot
-        console.error("fail to connect", err);
+
+// Create a table with name and columns
+app.post('/createTable' , async (req, res) =>{
+    const { tableName, columns } = req.body;
+    const columnQuery = columns.map(c => {
+        return `${c.key} ${c.type}`
+    }).join(', ')
+    var sql = `CREATE TABLE ${tableName} (${columnQuery})`;
+    const queryRes = await queryDB(sql);
+    
+    if (queryRes.error) {
+        res.send({
+            status: queryRes.code,
+            message: queryRes.error
+        })
     } else {
-        pg_client = client;
+        res.send({ 
+            status: 201, 
+            message: `Table ${tableName} has been created`
+        })
     }
-});
-
-// Get list folders
-app.get('/', function (_, res) {
-    var sql = "select * from doc_folders";
-    handleQgQuery(sql, res)
+    res.end();
 })
 
-// Get folder by id
-app.get('/:id', function (req, res) {
-    var sql = `select * from doc_folders where id = ${req.params.id}`;
-    handleQgQuery(sql, res)
+// Get list records by table name
+app.get('/:table', async (req, res) => {
+    var sql = `SELECT * FROM ${req.params.table}`;
+    const queryRes = await queryDB(sql);
+
+    if (queryRes.error) {
+        res.send({
+            status: queryRes.code,
+            message: queryRes.error
+        })
+    } else {
+        res.send(queryRes)
+    }
+    res.end()
 })
 
-// Update folder
-app.put('/:id', function (req, res) {
-    console.log('_____', req.params.id)
-    var sql = `update doc_folders
-                set folder_name = ${req.body.folder_name}
-                where id = ${req.params.id}`
-    handleQgQuery(sql, res)
+// Get record by id in table name
+app.get('/:table/:id', async (req, res) => {
+    var sql = `SELECT * FROM ${req.params.table} WHERE id = ${req.params.id}`;
+    const queryRes = await queryDB(sql);
+
+    if (queryRes.error) {
+        res.send({
+            status: queryRes.code,
+            message: queryRes.error
+        })
+    } else {
+        res.send(queryRes)
+    }
+    res.end()
 })
 
-function handleQgQuery(sql, res) {
-    var query = { text: sql, values: [] };
-    pg_client.query(query, function (err, result) {
-        if (err) {
-            console.error(err)
-            res.status(400);
-            res.write(JSON.stringify({ status: false, error: err }, null, 2));
-            res.end();
-        } else {
-            res.write(JSON.stringify({ status: true, result: result.rows }, null, 2));
-            res.end();
+// Insert record into table
+app.post('/:table', async (req, res) => {
+    console.log(req.body)
+    const keys = Object.keys(req.body);
+    const values = Object.values(req.body);
+    const placeholders = keys.map((_, idx) => `$${idx + 1}`).join(", ");
+    const sql = `INSERT INTO ${req.params.table} (${keys.join(", ")}) VALUES (${placeholders})`;
+    const queryRes = await queryDB(sql, values);
+
+    if (queryRes.error) {
+        res.send({
+            status: queryRes.code,
+            message: queryRes.error
+        })
+    } else {
+        res.send({message: 'New record added successfully'})
+    }
+    res.end()
+})
+
+// Update record by table name & id
+// app.put('/:table/:id', function (req, res) {
+//     console.log(req.body)
+//     const sql = `SELECT * FROM ${req.params.table} WHERE id = ${req.params.id}`;
+//     const queryRes = queryDB(sql);
+//     console.log("========", queryRes)
+//     const body = req.body;
+//     if (queryRes.error) {
+//         res.send({status: 400, error: queryRes.error})
+//         res.end();
+//         return;
+//     }
+// })
+
+async function queryDB(sql, values = []) {
+    try {    
+        const result = await client.query(sql, values);
+        console.log('989898989898989',result)
+        return { data: result?.rows, total: result?.rowCount }
+    } catch (error) {
+        switch (error.code) {
+            case '3D000': // Database không tồn tại
+                return { error: 'Database does not exist', code: error.code }
+            case '42P01': // Bảng không tồn tại
+                return { error: 'Record does not exist', code: error.code }
+            case '28000': // Authentication Failed
+                return { error: 'Authentication Failed', code: error.code }
+            case '42P07': // Bảng đã tồn tại
+                return { error: 'Table already exists', code: error.code }
+            case 'ECONNREFUSED': // Không thể kết nối
+                return { error: 'Something went wrong', code: error.code }
+            default:
+                return { error: error.message, code: error.code }
         }
-    })
+    }
 }
 
 app.listen(PORT, function () {
